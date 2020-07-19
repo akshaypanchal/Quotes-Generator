58ab3dc2ed649c79d6e1059fb2b950508791710d
Akshay <akshaypanchal67@gmail.com>
2020-07-19 00:41:15 -0400

    git commit -m "Final Commit, added Event Listeners to Buttons for API Fetching and Quotes Change"

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuotebtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
+// Show Loading
+function loading(){
+    loader.hidden =  false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete(){
+    if(!loader.hidden){
+        quoteContainer.hidden =  false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote from API
 async function getQuote(){
+
+    loading();
+
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl="http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json ";
 
@@ -33,6 +50,8 @@ async function getQuote(){
         }
         quoteText.innerText= data.quoteText;
 
+        // Stop Loader, Show Quote
+        complete();
     }
     catch(error){
         getQuote();
@@ -40,5 +59,19 @@ async function getQuote(){
     }
 }
 
+//Tweet Quote
+
+function tweetQuote(){
+    const quote=quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+
+}
+
+// Event Listeners
+
+newQuotebtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 // on Load
-getQuote();
